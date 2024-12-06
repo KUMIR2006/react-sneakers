b07f1a592ef8e585e1790cf9beb6abeec67ea64d
KUMIR2006 <kurbanovmirzobek@gmail.com>
2024-12-06 16:25:44 +0300

    Onclick И разделение scss кода

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import Card from "./components/Сard";
+import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
+const arr = [
+  {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: "/img/sneakers/boots1.jpg"},
+  {title: "Мужские Кроссовки Nike Air Max 270", price: 16599, imageUrl: "/img/sneakers/boots2.jpg"},
+  {title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imageUrl: "/img/sneakers/boots1.jpg"},
+]
+
 function App() {
   return (
 <div className="wrapper clear">
@@ -17,11 +24,14 @@ function App() {
       </div>
 
       <div className="d-flex">
-
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        {arr.map((obj) => (
+          <Card
+          title={obj.title}
+          price={obj.price}
+          imageUrl={obj.imageUrl}
+          onClick={() => console.log(obj)}
+          />
+        ))}
       </div>
       
 
