44659422a1b1e86daebf7222eb423434b433a1b8
KUMIR2006 <kurbanovmirzobek@gmail.com>
2024-12-08 01:36:06 +0300

    Getting data from the backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
-const arr = [
-  {title: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999, imageUrl: "/img/sneakers/boots1.jpg"},
-  {title: "Мужские Кроссовки Nike Air Max 270", price: 16599, imageUrl: "/img/sneakers/boots2.jpg"},
-  {title: "Кроссовки Puma X Aka Boku Future Rider", price: 8999, imageUrl: "/img/sneakers/boots1.jpg"},
-]
 
 function App() {
+  const [items, setItems] = React.useState([])
   const [cartOpened, setCartOpened] = React.useState(false);
   
+  React.useEffect(() => {
+  fetch('https://6754b7c836bcd1eec851d5bd.mockapi.io/items')
+      .then(res => { 
+        return res.json()
+      })
+      .then((json) => {
+        setItems(json)
+      });
+  }, []);
+
+
   return (
   <div className="wrapper clear">
   {cartOpened && <Drawer  onClose={() => setCartOpened(false)}/>}
   <Header onClickCart={() => setCartOpened(true)}/>
   <div className="content p-40">
-      <div className="d-flex align-center justify-between mb-40">
+      <div className="d-flex align-center justify-between mb-15">
         <h1>Все кроссовки</h1>
         <div className="search d-flex">
           <img src="/img/search.svg" alt="Search"/>
@@ -25,8 +32,8 @@ function App() {
         </div>
       </div>
 
-      <div className="d-flex">
-        {arr.map((obj) => (
+      <div className="d-flex flex-wrap">
+        {items.map((obj) => (
           <Card
           title={obj.title}
           price={obj.price}
